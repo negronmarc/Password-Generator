51696238e0d44905230c38d67561d81d3bc461c4
negronmarc <92690027+negronmarc@users.noreply.github.com>
2021-11-06 19:19:18 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Password Generator
+
+The goal of this project was to create an application that enables users to generate random passwords based on the criteria that they have selected. This application will run in the browser and has a responsive user interface that adapts to multiple screen sizes.
+
+<img src="./assets/images/preview.png" id="search" alt="Preview of the website" />
+
+https://negronmarc.github.io/passwordgenerator/
---
+++ assets/js/script.js
@@ -0,0 +1,111 @@
+console.log("js is working");
+
+// ==============
+// DATA
+// ==============
+
+var generatePasswordIDBtn = document.getElementById("generate");
+
+var specialCharStr = "~`!@$%^&*()_+-=";
+var upperCaseLetterStr = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var lowerCaseChars = "abcdefghijklmnopqrstuvwxy";
+var numbersStr = "0123456789";
+
+var specialCharArr = specialCharStr.split("");
+var upperCaseCharArr = upperCaseLetterStr.split("");
+var lowerCaseCharArr = lowerCaseChars.split("");
+var numbersArr = numbersStr.split("");
+
+// Array to hold all character sets confirmed by user
+var userCharPool = [];
+
+// Array to hold all the chars for the final password.
+var aPassword = [];
+
+// TODO: create an array variable to store the guaranteed chars - one char from each character set which user has confirmed
+
+var guaranteedCharsArr = [];
+
+// ================
+// MAIN PROCESS
+// ================
+
+function generatePassword () {
+    var userChoiceLength = prompt("Enter password length: ");
+        console.log(userChoiceLength);
+
+    var userChoiceSpecialChars = confirm("Special Characters?");
+        console.log(userChoiceSpecialChars);
+
+    var userChoiceUpperCase = confirm("Upper Case?");
+        console.log(userChoiceUpperCase);
+
+    var userChoiceLowerCase = confirm("Lower Case?");
+        console.log(userChoiceLowerCase);
+
+    var userChoiceNumbers = confirm("Numbers?");
+        console.log(userChoiceNumbers);
+
+  function copyArrayToPool(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      userCharPool.push(arr[i]);
+    }
+    console.log(userCharPool);
+    return;
+  }
+
+  if (userChoiceSpecialChars) {
+    copyArrayToPool(specialCharArr);
+    guaranteedCharsArr = Math.floor(Math.random() * specialCharStr.length);
+  }
+
+  if (userChoiceUpperCase) {
+    userCharPool = userCharPool.concat(upperCaseCharArr);
+    guaranteedCharsArr = Math.floor(Math.random() * upperCaseLetterStr.length);
+    }
+
+  if (userChoiceLowerCase) {
+    copyArrayToPool(lowerCaseCharArr);
+    guaranteedCharsArr = Math.floor(Math.random() * lowerCaseChars.length);
+  }
+
+  if (userChoiceNumbers) {
+    userCharPool = userCharPool.concat(numbersArr);
+    guaranteedCharsArr = Math.floor(Math.random() * numbersStr.length);
+  };
+
+  for (var i = 0; i < userChoiceLength; i++) {
+    var index = Math.floor(Math.random() * userCharPool.length);
+
+    console.log(userCharPool[index]);
+
+    aPassword.push(userCharPool[index]);
+
+  }
+
+  console.log(aPassword);
+  
+  aPassword.splice(0,1, guaranteedCharsArr);
+
+  aPassword.splice(1,1, guaranteedCharsArr);
+
+  finalPassword = aPassword.join("");
+  
+  console.log(finalPassword);
+
+  return finalPassword;
+
+};
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordTextEl = document.getElementById("password");
+
+  passwordTextEl.value = password;
+
+  return;
+
+}
+
+generatePasswordIDBtn.addEventListener("click", writePassword);
+
